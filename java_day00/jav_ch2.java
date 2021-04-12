6ca95b73e4bc134b6575d2542636eb003c481f62
paul_mauer <paulmauer37@gmail.com>
2021-04-12 23:48:56 +0900

    ch2, ch3 summary code

---
+++ java_day00/jav_ch2.java
@@ -0,0 +1,92 @@
+package java_day00;
+
+public class jav_ch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//		byte var1=125;
+//		int var2=125;
+//		for (int i=0; i<5; i++) {
+//			var1++;
+//			var2++;
+//			System.out.println("var1: "+var1+"\t"+"var2: "+var2);	// byte타입의 garbage value 발생
+//		}
+
+		System.out.println("-----------------------");
+		
+//		char c1 = 'A';
+//		char c2 = 65;
+//		char c3 = '\u0041';
+//		
+//		char c4 = '가';
+//		char c5 = 44032;
+//		char c6 = '\uac00';
+//		
+//		int uniCode1 = c1;
+//		int uniCode2 = c2;
+//		int uniCode3 = c3;
+//		int uniCode4 = c4;
+//		int uniCode5 = c5;
+//		int uniCode6 = c6;
+//		
+//		System.out.println(c1);
+//		System.out.println(c2);
+//		System.out.println(c3);
+//		System.out.println(c4);
+//		System.out.println(c5);
+//		System.out.println(c6);
+//		System.out.println(uniCode1);
+//		System.out.println(uniCode2);
+//		System.out.println(uniCode3);
+//		System.out.println(uniCode4);
+//		System.out.println(uniCode5);
+//		System.out.println(uniCode6);
+		
+		System.out.println("-----------------------");
+
+//		long var1 = 10;
+//		long var2 = 20L;
+////		long var3 = 100000000000000;	컴파일 에러!!!!!!!!
+//		long var4 = 100000000000000L;
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+////		System.out.println(var3);		컴파일 에러!!!!!!!!
+//		System.out.println(var4);
+		
+		System.out.println("-----------------------");
+		
+//		double var1 = 3.14;
+////		float var2 = 3.14;		컴파일 에러!!!!!!!!
+//		float var3 = 3.14f;
+//		
+//		double var4 = 0.1234567890123456789;
+//		float var5 = 0.1234567890123456789f;
+//		
+//		System.out.println("var1: "+var1);
+////		System.out.println("var1: "+var2);		컴파일 에러!!!!!!!!
+//		System.out.println("var1: "+var3);
+//		System.out.println("var1: "+var4);
+//		System.out.println("var1: "+var5);
+//		
+//		int var6 = 3000000;
+//		double var7 = 3e6;
+//		float var8 = 3e6F;
+//		double var9 = 2e-3;
+//		System.out.println("var6: "+var6);
+//		System.out.println("var7: "+var7);
+//		System.out.println("var8: "+var8);
+//		System.out.println("var9: "+var9);
+
+		System.out.println("-----------------------");
+
+//		boolean stop = true;
+		boolean stop = false;
+		if (stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}
+	}
+}
---
+++ java_day00/jav_ch3.java
@@ -0,0 +1,45 @@
+package java_day00;
+
+public class jav_ch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = -100;
+		int result1 = +x;
+		int result2 = -x;
+		System.out.println("result1=" + result1);
+		System.out.println("result2=" + result2);
+		
+		short s = 100;
+//		short result3 = -s;
+		int result3 = -s;
+		System.out.println("result3" + result3);
+
+		long y = 500;
+//		int result4 = -y;
+		long result4 = -y;
+		System.out.println("result4" + result4);
+		
+		String a = "아나콘다";
+//		string b = 'anaconda';		// 컴파일 에러 String, "" 유의!
+		System.out.println(a);
+//		System.out.println(b);
+		
+		String strVar1 = "신용권";
+		String strVar2 = "신용권";
+		String strVar3 = new String("신용권"); 
+		boolean k_1 = (strVar1 == strVar2);
+		boolean k_2 = (strVar1 == strVar3);
+//		for (int i=1; i<5; i++) {
+//			int num = 0;
+//			num++;	
+//			if((k_+'num') = true) {
+//				System.out.println("같다");
+//			} else {
+//				System.out.println("다르다");
+//			}	
+//		}
+		
+	}
+
+}
